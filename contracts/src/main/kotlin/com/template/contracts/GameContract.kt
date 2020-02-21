ab0c2b239cdc86b71db261053a00acf1f7200359
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2020-02-21 10:20:57 +0000

    Remove unused commands

--- contracts/src/main/kotlin/com/template/contracts/GameContract.kt
+++ contracts/src/main/kotlin/com/template/contracts/GameContract.kt
@@ -37,36 +37,6 @@ class GameContract : Contract {
                 "p2SetPos must be false" using (!gState.p2SetPos)
                 "turn must be p1" using (gState.turn == gState.p1)
             }
-            is Commands.P1SetPos -> requireThat {
-                "There should be one input state" using (tx.inputs.count() == 1)
-                "There should be 101 output states" using (tx.outputs.count() == 101)
-                "The input state must be of type ${GameState::class.simpleName}" using (tx.inputs[0].state.data is GameState)
-                "The first output state must be of type ${GameState::class.simpleName}" using (tx.outputs[0].data is GameState)
-                "The last 100 output states must be of type ${PositionState::class.simpleName}" using (tx.outputs.asSequence().drop(1).all { it.data is PositionState })
-                val gState = tx.inputs[0].state.data as GameState
-                val positions = tx.outputs.asSequence().drop(1).map { it.data as PositionState }
-                "The number of set positions must match maxScore" using (positions.filter { it.containsShip }.count() == gState.maxScore)
-                "It is P1 turn" using (gState.turn == gState.p1)
-                "All positions belong to P1" using (positions.all { it.owner == gState.p1 && it.originallyOwnedBy == gState.p1 })
-                val outputGState = tx.outputs[0].data as GameState
-                "The turn should move to P2" using (outputGState.turn == outputGState.p2)
-                "Positions are set for P1" using (outputGState.p1SetPos)
-            }
-            is Commands.P2SetPos -> requireThat {
-                "There should be one input state" using (tx.inputs.count() == 1)
-                "There should be 101 output states" using (tx.outputs.count() == 101)
-                "The input state must be of type ${GameState::class.simpleName}" using (tx.inputs[0].state.data is GameState)
-                "The first output state must be of type ${GameState::class.simpleName}" using (tx.outputs[0].data is GameState)
-                "The last 100 output states must be of type ${PositionState::class.simpleName}" using (tx.outputs.asSequence().drop(1).all { it.data is PositionState })
-                val gState = tx.inputs[0].state.data as GameState
-                val positions = tx.outputs.asSequence().drop(1).map { it.data as PositionState }
-                "The number of set positions must match maxScore" using (positions.filter { it.containsShip }.count() == gState.maxScore)
-                "It is P2 turn" using (gState.turn == gState.p2)
-                "All positions belong to P2" using (positions.all { it.owner == gState.p2 && it.originallyOwnedBy == gState.p2 })
-                val outputGState = tx.outputs[0].data as GameState
-                "The turn should move to P1" using (outputGState.turn == outputGState.p1)
-                "Positions are set for P2" using (outputGState.p2SetPos)
-            }
             is Commands.SetPos -> requireThat {
                 "There should be one input state" using (tx.inputs.count() == 1)
                 "There should be 101 output states" using (tx.outputs.count() == 101)
@@ -113,8 +83,6 @@ class GameContract : Contract {
     interface Commands : CommandData {
         class New : Commands
         class SetPos : Commands
-        class P1SetPos : Commands
-        class P2SetPos : Commands
         class P1Turn : Commands
         class P2Turn : Commands
     }
