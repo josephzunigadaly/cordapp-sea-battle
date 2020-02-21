11ebd0a33a25bd475eae31edae4a19057fb0b224
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2020-02-21 16:38:15 +0000

    Implement TurnFlow

--- workflows/src/main/kotlin/com/template/flows/TurnFlow.kt
+++ workflows/src/main/kotlin/com/template/flows/TurnFlow.kt
@@ -11,6 +11,10 @@ import net.corda.core.identity.Party
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
+import net.corda.core.utilities.UntrustworthyData
+import net.corda.core.utilities.unwrap
+
+data class Params(val gameName: String, val coordinate: String)
 
 // *********
 // * Flows *
@@ -23,88 +27,73 @@ class TurnInitiator(
 ) : FlowLogic<SignedTransaction>() {
 
     companion object {
-//        object COLLECTING : ProgressTracker.Step("Collecting signatures from counterparties.")
-//        object VERIFYING : ProgressTracker.Step("Verifying collected signatures.")
-
-//        @JvmStatic
-//        fun tracker() = ProgressTracker(COLLECTING, VERIFYING)
+        object FINDING : ProgressTracker.Step("Asking other player for coordinate.")
+        object TRANSFERRING : ProgressTracker.Step("Finalising transfer.")
     }
-    override val progressTracker = ProgressTracker()
+    override val progressTracker = ProgressTracker(FINDING, TRANSFERRING)
 
     @Suspendable
     override fun call() : SignedTransaction {
 
-//        val gameState = serviceHub.vaultService.queryBy(GameState::class.java).states.single { it.state.data.name == gameName }
-//        val game = gameState.state.data
-//
-//        // Initiator flow logic goes here.
-//        val notary = serviceHub.networkMapCache.notaryIdentities.first()
-//        val command = Command(GameContract.Commands.SetPos(), listOf(ourIdentity).map(Party::owningKey))
-//
-//        // Build output game state
-//        val outputGameState: GameState
-//        outputGameState = if (game.p1 == ourIdentity){
-//            game.copy(p1SetPos = true, turn = game.p2)
-//        } else {
-//            game.copy(p2SetPos = true, turn = game.p1)
-//        }
-//
-//        // Empty grid
-//        val map = mutableMapOf<String, PositionState>()
-//        for (x in 'A'..'I'){
-//            for (y in 0..9){
-//                val pos = "${x}${y}"
-//                map[pos] = PositionState(ourIdentity, gameName, pos, false)
-//            }
-//        }
-//
-//        // Read ship locations and add to map
-//        val regex = Regex.fromLiteral("([A-Ia-i])([0-8])([NESWnesw])")
-//
-//        for ((ship, size) in listOf(
-//                Pair(aircraftCarrier, 5),
-//                Pair(battleship, 4),
-//                Pair(cruiser, 3),
-//                Pair(destroyer1, 2),
-//                Pair(destroyer2, 2),
-//                Pair(submarine1, 1),
-//                Pair(submarine2, 1)
-//        )) {
-//            val matchResult = regex.matchEntire(ship)!!
-//
-//            val col = matchResult.groups[0]!!.value[0].toUpperCase()
-//            val row = Integer.parseInt(matchResult.groups[1]!!.value)
-//            val direction = matchResult.groups[2]!!.value.toUpperCase()
-//
-//            for (i in 0 until size){
-//                val posName: String = when (direction){
-//                    "N" -> "$col${row - i}"
-//                    "E" -> "${(col + i)}$row"
-//                    "S" -> "$col${row + i}"
-//                    "W" -> "${(col - i)}$row"
-//                    else -> throw IllegalArgumentException("Direction needs to be one of N, E, S, W")
-//                }
-//
-//                // Check position already exists
-//                requireThat { "Ship positioned outside grid" using (map.containsKey(posName)) }
-//
-//                // Update the map
-//                map[posName] = PositionState(ourIdentity, gameName, posName, true)
-//            }
-//        }
-//
-//        // Build up the transaction
-//        val transactionBuilder = TransactionBuilder(notary)
-//                .addCommand(command)
-//                .addInputState(gameState)
-//                .addOutputState(outputGameState, GameContract.ID)
-//        for (p in map.values){
-//            transactionBuilder.addOutputState(p, GameContract.ID)
-//        }
-//        transactionBuilder.verify(serviceHub)
-//        val transaction = serviceHub.signInitialTransaction(transactionBuilder)
-//
-//        return subFlow(FinalityFlow(transaction, emptyList()))
-        TODO()
+        progressTracker.currentStep = FINDING
+
+        val gameState = serviceHub.vaultService.queryBy(GameState::class.java).states.single { it.state.data.name == gameName }
+        val game = gameState.state.data
+
+        val otherPlayer = if (game.p1 == ourIdentity) game.p2 else game.p1
+
+        val initiateFlow = initiateFlow(otherPlayer)
+        val transaction = initiateFlow.sendAndReceive<SignedTransaction>(Params(gameName, coordinate)).unwrap{it}
+
+        progressTracker.currentStep = TRANSFERRING
+
+        transaction.verify(serviceHub)
+        requireThat { "Returned position is not the one we requested" using ((transaction.tx.outputStates[0] as PositionState).positionName == coordinate) }
+        serviceHub.addSignature(transaction)
+
+        return subFlow(FinalityFlow(transaction, initiateFlow))
     }
+}
+
+@InitiatedBy(TurnInitiator::class)
+class TurnResponder(val counterpartySession: FlowSession) :FlowLogic<Unit>() {
+
+    companion object {
+        object FINDING : ProgressTracker.Step("Finding position in vault.")
+        object TRANSFERRING : ProgressTracker.Step("Transferring position to other player.")
+    }
+    override val progressTracker = ProgressTracker(FINDING, TRANSFERRING)
+
+    @Suspendable
+    override fun call() {
+        val receiveAll = receiveAll<Params>(Params::class.java, listOf(counterpartySession))
+        val (gameName, coordinate) = receiveAll[0].unwrap { it }
+
+        progressTracker.currentStep = FINDING
+
+        val gameState = serviceHub.vaultService.queryBy(GameState::class.java).states.single { it.state.data.name == gameName }
+        val game = gameState.state.data
+
+        val otherPlayer = if (game.p1 == ourIdentity) game.p2 else game.p1
+
+        val gamePositionStates = serviceHub.vaultService.queryBy(PositionState::class.java).states.filter { it.state.data.gameName == gameName }
+        val ourPositions = gamePositionStates.filter { it.state.data.originallyOwnedBy == ourIdentity }
+        val desiredPos = ourPositions.single {it.state.data.positionName == coordinate}
+
+        progressTracker.currentStep = TRANSFERRING
+
+        val newPosition = desiredPos.state.data.copy(owner = otherPlayer)
+        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+        val command = Command(GameContract.Commands.Turn(), listOf(ourIdentity, otherPlayer).map(Party::owningKey))
+
+        val transactionBuilder = TransactionBuilder(notary)
+                .addInputState(desiredPos)
+                .addOutputState(newPosition, GameContract.ID)
+                .addCommand(command)
+        transactionBuilder.verify(serviceHub)
+        val transaction = serviceHub.signInitialTransaction(transactionBuilder)
+
+        counterpartySession.send(transaction)
+    }
+
 }
